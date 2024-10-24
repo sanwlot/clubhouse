52f9ebb0b843f85abcc1848d7a9cc24f39be7eee
sunnySavlot <sunny.savlot@gmail.com>
2024-10-24 16:33:23 +0530

    author name added in the messages feed

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,16 +4,21 @@ module.exports = {
   getIndex: async (req, res) => {
     let messages
     try {
-      const { rows } = await pool.query("SELECT * FROM messages;")
-      messages = rows.map((message) => {
-        const createdAt = new Date(message.created_at)
-        return {
-          title: message.title,
-          message: message.message,
-          userId: message.user_id,
-          createdAt: createdAt.toDateString(),
-        }
-      })
+      const { rows } = await pool.query(
+        " SELECT messages.id, messages.title, messages.message, messages.created_at, users.first_name, users.last_name FROM messages JOIN users ON messages.user_id = users.id;"
+      )
+      messages = rows
+        .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+        .map((message) => {
+          const createdAt = new Date(message.created_at)
+          return {
+            title: message.title,
+            message: message.message,
+            userId: message.user_id,
+            author: message.first_name + " " + message.last_name,
+            createdAt: createdAt.toDateString(),
+          }
+        })
     } catch (error) {
       console.log(error)
       messages = []
